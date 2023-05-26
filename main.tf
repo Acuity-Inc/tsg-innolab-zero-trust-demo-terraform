4e39b095fc11254e2e2611cdbaaec2999e791a4f
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-05-26 14:20:26 -0400

    Implemented Jenkinsfile to fetch env variables from AWS SSM. Executing terraform scripts from Jenkinsfile

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 terraform {
+  required_version = "~> 1.4.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,21 +11,15 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = var.s3_bucket
-    key    = var.s3_key
-    region = var.s3_region
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
   region = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
 }
 
 provider "okta" {
-  org_name  = var.okta_org_name
-  base_url  = var.okta_base_url
+  org_name  = "trial-6669138"
+  base_url  = "okta.com"
   api_token = var.okta_api_token
 }
