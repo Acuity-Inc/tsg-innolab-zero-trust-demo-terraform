ac19c3524750341c5cee57a19c5fb92deb0ebc9c
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-05-26 21:31:58 -0400

    Removed unused variables

--- variables.tf
+++ variables.tf
@@ -20,16 +20,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "aws_access_key" {
-  description = "AWS access key"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "AWS secret key"
-  type        = string
-}
-
 
 variable "okta_api_token" {
   description = "Okta API token"
